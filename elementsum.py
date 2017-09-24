2b230a6710da4913a8d207b487f559d680e5ba22
Sarbesh Kumar Sarkar <sarbeshsarkarsks@gmail.com>
2017-09-24 14:26:36 +0530

    find sum of N elements in a string
    Given an array of N integers, can you find the sum of its elements?

---
+++ elementsum.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+import sys
+
+def simpleArraySum(n, ar):
+    return sum(ar)
+        
+
+n = int(raw_input().strip())
+ar = map(int, raw_input().strip().split(' '))
+result = simpleArraySum(n, ar)
+print(result)
