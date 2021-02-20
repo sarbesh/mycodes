042dd320221573cd0aabc13c98462f110231c5b8
sarbesh <sarbeshsarkarsks@gmail.com>
2021-02-20 21:08:23 +0530

    Added TCS digital 20/02

---
+++ java/src/com/tcs/ion/strOP.java
@@ -0,0 +1,21 @@
+//given a string find number of operations(remove, replace, insert) 
+//that can be performed on it to get second given string
+//Example:
+//input: 	morning						convent
+//			bring						contant
+//output	3							2
+//			remove 'm'					convert 'v' -> 't'
+//			replace 'o' -> 'b'			convert 'e' -> 'a'
+//			remove 'n'
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class strOP{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.nextLine();
+		String B = sc.nextLine();
+	}
+}
